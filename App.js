137de37e8a5c81aa3bf33e054a1733809ef3aec8
Kenneth <kendevops@gmail.com>
2020-07-04 21:20:01 +0100

    Changed n to num in the dataurl function

--- App.js
+++ App.js
@@ -226,11 +226,11 @@ function LabelsIdentification() {
       var arr = dataurl.split(","),
         mime = arr[0].match(/:(.*?);/)[1],
         bstr = atob(arr[1]),
-        n = bstr.length,
-        u8arr = new Uint8Array(n);
+        num = bstr.length,
+        u8arr = new Uint8Array(num);
 
-      while (n--) {
-        u8arr[n] = bstr.charCodeAt(n);
+      while (num--) {
+        u8arr[num] = bstr.charCodeAt(n);
       }
 
       return new File([u8arr], filename, { type: mime });
